c5cbe020c6396a2962691d07e43e3ecf3d1c5aed
MattMiller1989 <57654274+MattMiller1989@users.noreply.github.com>
2020-04-21 10:29:44 -0500

    Update Binary-Tree.rb

--- Binary-Tree.rb
+++ Binary-Tree.rb
@@ -22,6 +22,25 @@ class Node
         return @value.to_s
     end
 
+    def is_leaf
+        if left == nil && right ==nil
+            return true
+        else
+            return false
+        end
+    end
+
+    def find_min
+        if @left==nil
+            return self
+        elsif left.left.is_leaf
+            return left.left
+        else
+           min= left.find_min
+        end
+        return min
+    end
+
 
 end
 
@@ -32,9 +51,9 @@ class Tree
     def initialize(arr)
         arr.sort!
         arr.uniq!
-        puts arr
+        #puts arr
         @root=build_tree(arr)
-        puts @root
+        #puts @root
     end
 
     def build_tree(arr)
@@ -54,7 +73,7 @@ class Tree
 
         parent.left=build_tree(left_arr)
         parent.right=build_tree(right_arr)
-        puts "left: #{parent.left} parent: #{parent} right: #{parent.right}"
+        #puts "left: #{parent.left} parent: #{parent} right: #{parent.right}"
         
 
         return parent
@@ -81,10 +100,66 @@ class Tree
         
             
     end
+    def delete(root=@root,value)
+        temp_node=Node.new(Value)
+
+        if root == nil
+            return root
+
+        elsif temp_node<root
+               root.left= delete(root.left,value)
+            elsif temp_node>root
+               root.right=delete(root.right,value)
+            
+        else
+            if root.is_leaf
+                root =null
+                
+    
+            elsif root.left==null
+                temp=root
+                root=root.right
+                temp = null
+                
+            elsif root.right==null
+                temp=root
+                root=root.left
+                temp = null
+                
+            else
+                temp  = root.right.find_min
+                root.value=temp.value
+                root.right=delete(root.right,temp.value)
+            end
+
+            
+
+            
+        end
+        
+
+
+    end
+
+    def preorder(root=@root)
+        if root == nil 
+            return
+        end
+        puts root
+        preorder(root.left)
+        preorder(root.right)
+
+    end
+
             
 
 end
 
 arr=[7,5,6,3,2,1,8,10]
 my_tree=Tree.new(arr)
+# my_tree.insert(4)
+
+my_tree.preorder(my_tree.root)
 my_tree.insert(4)
+my_tree.preorder
+
